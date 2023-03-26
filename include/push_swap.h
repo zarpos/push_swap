550933f25cfc49a0a284f93e393acae0db6585df
Diego Rubio morato <drubio-m@c2r15s3.42madrid.com>
2023-03-26 13:31:37 +0200

    push_swap

---
+++ include/push_swap.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drubio-m <drubio-m@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/26 11:56:30 by drubio-m          #+#    #+#             */
+/*   Updated: 2023/03/26 11:57:23 by drubio-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+# include "./libft/libft.h"
