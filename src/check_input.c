8a76a6384c37543e350610448db61107a2d771c3
Diego Rubio morato <drubio-m@c2r15s2.42madrid.com>
2023-04-03 17:33:17 +0200

    normi

--- src/check_input.c
+++ src/check_input.c
@@ -6,29 +6,29 @@
 /*   By: drubio-m <drubio-m@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 00:41:42 by drubio-m          #+#    #+#             */
-/*   Updated: 2023/03/29 20:05:59 by drubio-m         ###   ########.fr       */
+/*   Updated: 2023/04/03 17:31:35 by drubio-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
 // It will check that the arguments are number. (1 = Error, 0 = Success)
-int check_num(char *str)
+int	check_num(char *str)
 {
-    int i;
-    int len;
+	int	i;
+	int	len;
 
-    i = 0;
-    len = ft_strlen(str);
-    if (((str[i] == '-' || str[i] == '+') && len > 1) || ft_isdigit(str[i]))
-    {
-        while (str[i])
-        {
-            i++;
-            if (!ft_isdigit(str[i]))
-                return (1);
-        }
-        return (0);
-    }
-    return (1);
+	i = 0;
+	len = ft_strlen(str);
+	if (((str[i] == '-' || str[i] == '+') && len > 1) || ft_isdigit(str[i]))
+	{
+		while (str[i])
+		{
+			i++;
+			if (!ft_isdigit(str[i]))
+				return (1);
+		}
+		return (0);
+	}
+	return (1);
 }
--- src/utils.c
+++ src/utils.c
@@ -6,20 +6,20 @@
 /*   By: drubio-m <drubio-m@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 16:32:38 by drubio-m          #+#    #+#             */
-/*   Updated: 2023/04/03 16:33:46 by drubio-m         ###   ########.fr       */
+/*   Updated: 2023/04/03 17:32:56 by drubio-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void free_array(char **str)
+void	free_array(char **str)
 {
-    int i;
-    
-    i = 0;
-    while (str[i])
-        i++;
-    while (i >= 0)
-    {
-        free(str[i])
-        i--;
-    }
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	while (i >= 0)
+	{
+		free(str[i]);
+		i--;
+	}
 }
