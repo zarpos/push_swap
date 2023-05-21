cdfd752593a2407718ea498d9eaaea9bb4c77ff7
Diego Rubio <diegorubio@Diego-2.local>
2023-05-21 12:10:57 +0200

    movs 65%

--- src/movs1.c
+++ src/movs1.c
@@ -6,7 +6,7 @@
 /*   By: drubio-m <drubio-m@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 18:31:19 by drubio-m          #+#    #+#             */
-/*   Updated: 2023/05/20 19:33:10 by drubio-m         ###   ########.fr       */
+/*   Updated: 2023/05/21 12:10:39 by drubio-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,27 +25,134 @@ typedef struct s_lista
 	struct s_lista	*next;
 }	t_lista; 
 
-void	sa(t_lista **stack)
+void	sa(t_lista **stack_a)
 {
-	t_lista *temp;
-	t_lista *temp2;
-
-	temp = *stack;
-	*stack = temp->next;
-	(*stack)->next = NULL;
-	temp2 = temp->next->next;
-	temp->next = NULL;
-	temp->next = temp2;
-	(*stack)->next = temp;
-
 	t_lista	*swap;
 	t_lista	*swap2;
 	
-	swap = *stack; //swap = 2 1 3
+	swap = *stack_a; //swap = 2 1 3
 	swap2 = swap->next; // swap2 = 1 y 3
-	*stack = swap2; // stack = 1 y 3
-	swap->next = swap2->next; // swa
+	*stack_a = swap2; // stack = 1 y 3
+	swap->next = swap2->next; // swap = 2 y 3
+    swap2->next = swap; // swap2 = 1 2 3
+	return ;
+}
+
+void	sb(t_lista **stack_b)
+{
+	t_lista	*swap;
+	t_lista	*swap2;
+	
+	swap = *stack_b;
+	swap2 = swap->next;
+	*stack_b = swap2;
+	swap->next = swap2->next;
     swap2->next = swap;
+	return ;
+}
+
+void	ss(t_lista **stack_a, t_lista **stack_b)
+{
+	t_lista	*swap;
+	t_lista	*swap2;
+	
+	swap = *stack_a;
+	swap2 = swap->next;
+	*stack_a = swap2;
+	swap->next = swap2->next;
+    swap2->next = swap;
+	swap = NULL;
+	swap2 = NULL;
+	swap = *stack_b;
+	swap2 = swap->next;
+	*stack_b = swap2;
+	swap->next = swap2->next;
+    swap2->next = swap;
+	return ;
+}
+
+void	pa(t_lista **stack_a, t_lista **stack_b)
+{
+	t_lista *temp;
+	
+	if (!*stack_b)
+		return ;
+	temp = *stack_b;
+	*stack_b = (*stack_b)->next;
+	temp->next = *stack_a;
+	*stack_a = temp;
+	return ;
+}
+
+void	pb(t_lista **stack_a, t_lista **stack_b)
+{
+	t_lista *temp;
+	
+	if (!*stack_a)
+		return ;
+	temp = *stack_a;
+	*stack_a = (*stack_a)->next;
+	temp->next = *stack_b;
+	*stack_b = temp;
+	return ;
+}
+
+void	ra(t_lista **stack_a)
+{
+	t_lista *temp;
+	
+	if (ft_lstsize(*stack_a) > 1)
+	{
+		temp = *stack_a;
+		*stack_a = (*stack_a)->next;
+		temp->next = NULL;
+		while(*stack_a)
+			(*stack_a)->next;
+		(*stack_a)->next = temp;
+	}
+	return ;
+}
+
+void	rb(t_lista **stack_b)
+{
+	t_lista *temp;
+	
+	if (ft_lstsize(*stack_b) > 1)
+	{
+		temp = *stack_b;
+		*stack_b = (*stack_b)->next;
+		temp->next = NULL;
+		while(*stack_b)
+			(*stack_b)->next;
+		(*stack_b)->next = temp;
+	}
+	return ;
+}
+
+void	rr(t_lista **stack_a, t_lista **stack_b)
+{
+	t_lista *temp;
+	
+	if (ft_lstsize(*stack_a) > 1)
+	{
+		temp = *stack_a;
+		*stack_a = (*stack_a)->next;
+		temp->next = NULL;
+		while(*stack_a)
+			(*stack_a)->next;
+		(*stack_a)->next = temp;
+	}
+	temp = NULL;
+	if (ft_lstsize(*stack_b) > 1)
+	{
+		temp = *stack_b;
+		*stack_b = (*stack_b)->next;
+		temp->next = NULL;
+		while(*stack_b)
+			(*stack_b)->next;
+		(*stack_b)->next = temp;
+	}
+	return ;
 }
 
 int main(void)
