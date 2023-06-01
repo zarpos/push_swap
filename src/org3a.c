32709379cdc85f001f80f6e1ebf6ab9df8e5e76e
Diego Rubio morato <drubio-m@c1r8s4.42madrid.com>
2023-06-01 16:58:03 +0200

    acabao

--- src/org3a.c
+++ src/org3a.c
@@ -6,7 +6,7 @@
 /*   By: drubio-m <drubio-m@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 21:41:42 by drubio-m          #+#    #+#             */
-/*   Updated: 2023/05/26 16:54:46 by drubio-m         ###   ########.fr       */
+/*   Updated: 2023/06/01 16:35:43 by drubio-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,13 @@ void	org3a(t_list **stack)
 	max = get_max(*stack);
 	if ((*stack)->value == min && (*stack)->next->value == max)
 	{
-		rra(stack, 'a');
 		sa(stack);
+		ra(stack, 'a');
 	}
 	else if ((*stack)->value == max && (*stack)->next->value != min)
 	{
-		sa(stack);
 		ra(stack, 'a');
+		sa(stack);
 	}
 	else if ((*stack)->value == max && (*stack)->next->value == min)
 		ra(stack, 'a');
